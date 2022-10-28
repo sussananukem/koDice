df1a245831fa70443d79122ddd3256065b4de60a
Sussan Anukem <40360782+sussananukem@users.noreply.github.com>
2022-10-28 20:54:25 +0000

    Responsiveness; Sound; and Delay added

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ element.addEventListener("click", myFunction);
 
 //Function to execute when the rolldice image is clicked
 function myFunction() {
+    //Getting random number generator for each dice
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
     //Animation to do when the dice is clicked: FIX BUG HERE LATER(Animate on each click)
     element.classList.add("animate__animated", "animate__rotateIn");
     //Remove it after 5 seconds
     setTimeout(
         function () {
             element.classList.remove("animate__animated", "animate__rotateIn");
+            //Selecting dice image to display based on random number generated
+            document.querySelector(".imgOne").setAttribute("src", "assets/images/dice" + randomNumber1 + ".png");
+            document.querySelector(".imgTwo").setAttribute("src", "assets/images/dice" + randomNumber2 + ".png");
         },
         500
     );
 
+    //Add sound
+    var sound = new Audio("assets/sound/dicesound.mp3");
+    sound.play();
 
-    //Getting random number generator for each dice
-    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-
-    //Selecting dice image to display based on random number generated
-    document.querySelector(".imgOne").setAttribute("src", "assets/images/dice" + randomNumber1 + ".png");
-    document.querySelector(".imgTwo").setAttribute("src", "assets/images/dice" + randomNumber2 + ".png");
 
     //What to output based on the output of the dice roll
     if (randomNumber1 === randomNumber2) {
@@ -36,6 +38,8 @@ function myFunction() {
         document.querySelector(".headerOne").textContent = "🚩 Player 1 Wins!";
     }
 }
+
+//Add Sound
 
 
 
